8d041c428d2d9b185083277b959f6c165c951052
Jenny Wu <syw034@ucsd.edu>
2020-02-13 20:54:00 -0800

    added fb log in

---
+++ routes/homepage.js
@@ -0,0 +1,34 @@
+
+/*
+ * GET home page.
+ */
+
+var course = require('../courses.json');
+
+exports.view = function(req, res){
+  res.render('homepage',course);
+};
+
+
+/*
+$(document).ready(function () {
+	$("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#dismiss, .overlay').on('click', function () {
+        // hide sidebar
+        $('#sidebar').removeClass('active');
+        // hide overlay
+        $('.overlay').removeClass('active');
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open sidebar
+        $('#sidebar').addClass('active');
+        // fade in the overlay
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});*/
